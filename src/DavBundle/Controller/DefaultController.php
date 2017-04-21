b9a7e0aa40ec5aa4ba56975a2ec426d9c6025320
David MOLINARI <david@rmolinari.net>
2017-04-21 23:53:09 +0200

    Add views

--- src/DavBundle/Controller/DefaultController.php
+++ src/DavBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('DavBundle:Default:index.html.twig');
+        return $this->render('@Dav/index.html.twig');
     }
 }
