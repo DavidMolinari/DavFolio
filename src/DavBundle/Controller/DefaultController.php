e2aeb1d4059dd296fb8068ba266494860c9e03f3
David MOLINARI <david@rmolinari.net>
2017-04-22 05:29:00 +0200

    form comp done

--- src/DavBundle/Controller/DefaultController.php
+++ src/DavBundle/Controller/DefaultController.php
@@ -2,8 +2,16 @@
 
 namespace DavBundle\Controller;
 
+use DavBundle\Entity\Competence;
+use DavBundle\Entity\David;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 class DefaultController extends Controller
 {
@@ -14,4 +22,36 @@ class DefaultController extends Controller
     {
         return $this->render('@Dav/index.html.twig');
     }
+
+
+
+    /**
+     * @Route("/generationcompetences")
+     */
+    public function generationCompetences(Request $request)
+    {
+
+        $comp = new Competence();
+
+        $form = $this->createFormBuilder($comp)
+            ->add('libelle', TextType::class, array('label' => 'Libelle comp :'))
+            ->add('value', IntegerType::class, array('label' => 'value :'))
+            ->add('save', SubmitType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($comp);
+            $em->flush();
+            return $this->render('@Dav/index.html.twig');
+        }
+        return $this->render('@Dav/Default/form.html.twig', array('form' => $form->createView()));
+
+    }
+
+
+
 }
