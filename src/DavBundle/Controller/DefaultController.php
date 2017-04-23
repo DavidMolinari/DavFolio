e06a5606fa43e5f7eaaca22971db113d84c8cefb
David MOLINARI <david@rmolinari.net>
2017-04-23 17:51:46 +0200

    Add img

--- src/DavBundle/Controller/DefaultController.php
+++ src/DavBundle/Controller/DefaultController.php
@@ -41,8 +41,6 @@ class DefaultController extends Controller
                                                             'projets' => $projets));
     }
 
-
-
     /**
      * @Route("/generationcompetences")
      */
